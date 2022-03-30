73e3ee0a5ef8dd955d86b2fae143480570429b87
nsydn <serhan.aydin@engr.istinye.edu.tr>
2022-03-30 23:07:24 +0300

    update

--- pi_est.m
+++ pi_est.m
@@ -8,20 +8,3 @@ for i=1:length(n)
 end
 hold all
 plot(n,pi_hat,'.-')
-
-%%
-x = -1:0.01:1;
-y = sqrt(1-x.^2);
-y0 = zeros(1,length(x));
-x0 = zeros(1,length(x));
-
-hold on
-plot(x,y,'b',x,-y,'b','LineWidth',2)
-plot(x,y0,'k',x0,x,'k')
-xlim([0 1])
-ylim([0 1])
-
-rng(1234); s = rng;
-scatter(rand(1000,1),rand(1000,1),'.r')
-
-grid on
