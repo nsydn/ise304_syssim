406cac6ba5aa3b3f9f027ad649331b4aad565f47
nsydn <serhan.aydin@engr.istinye.edu.tr>
2022-03-30 22:52:56 +0300

    Create mall_parking.m

---
+++ mall_parking.m
@@ -0,0 +1,12 @@
+lambda=40;
+mu=1/2;
+
+n=0;
+%p=exp(-lambda/mu)*(lambda/mu)^n/factorial(n);
+pf=@(n)exp(-lambda/mu)*(lambda/mu)^n/factorial(n);
+p=pf(0);
+while p<=0.99
+    n=n+1;
+    p=p+pf(n);
+end
+p,n
